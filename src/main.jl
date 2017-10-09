3812a29328c425559f6fa902bb6e75f2b36c9340
Stefano Battaglia <stefano.battaglia@irsamc.ups-tlse.fr>
2017-10-09 16:13:14 +0200

    ab-initio parte rewritten, details moved into suppinfo

--- src/main.jl
+++ src/main.jl
@@ -7,19 +7,19 @@ include("xyz_parser.jl")
 ϵ_C_N_int = 3.536; r0_C_N_int = 3.818;      # Carbon to internal nitrogen
 ϵ_H_N_ext = 2.827; r0_H_N_ext = 3.644;
 ϵ_H_N_int = 2.431; r0_H_N_int = 3.348;
-β = 7.0
+β = 9.0
 m = 6.0
 Q_N_ext = -0.56; Q_N_int = +0.12; # in au units
 α_C = 1.2 # in Ang^3
 α_H = 0.38
 
 # Read the two fragments
-frag_1 = readxyz("data/geometries/t553_b97d3_cc-pvtz.xyz")
+frag_1 = readxyz("data/geometries/t5513_b97d3_cc-pvtz.xyz")
 frag_2 = readxyz("data/geometries/n3_b97d3_aug-cc-pvtz.xyz")
 
 # Parse atomic charges of fragment_1 and set them
 # frag_1.charges = atomic_charges("data/t553_b97d3_cc-pvtz.log")
-frag_1.charges = readdlm("data/charges/t553_b97d3_cc-pvtz_npa.dat")
+frag_1.charges = readdlm("data/charges/t5513_b97d3_cc-pvtz_npa.dat")
 
 # Set partial atomic charges to azide anion
 frag_2.charges = [Q_N_ext Q_N_int Q_N_ext]
